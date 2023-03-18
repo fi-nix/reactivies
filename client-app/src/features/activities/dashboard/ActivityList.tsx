c29c03e48ffb8328ceef142f2f1a2edc293a4a55
rajthilak <rajthilak.yuva@gmail.com>
2023-03-18 21:14:07 +0530

    UI chnages

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,57 +1,26 @@
 import { observer } from "mobx-react-lite";
-import React, { SyntheticEvent, useState } from "react";
-import { Link } from "react-router-dom";
-import { Button, Item, ItemExtra, Label, Segment } from "semantic-ui-react";
+import { Fragment } from "react";
+import { Header, Item, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
+import ActivityListItem from "./ActivityListItem";
 
 
 export default observer(function ActivityList() {
-
-    const [target, setTarget] = useState('');
-
     const { activityStore } = useStore();
-
-    const { deleteActivity, ActivitiesByDate, loading } = activityStore;
-
-    function handleDeleteActivity(e: SyntheticEvent<HTMLButtonElement>, id: string) {
-        setTarget(e.currentTarget.name);
-        deleteActivity(id);
-    }
-
+    const { groupedActivities } = activityStore;
 
     return (
-        <Segment>
-            <Item.Group divided>
-                {ActivitiesByDate.map(activity => (
-                    <Item key={activity.id}>
-                        <Item.Content>
-                            <Item.Header as='a'>{activity.title}</Item.Header>
-                            <Item.Meta>{activity.date}</Item.Meta>
-                            <Item.Description>
-                                <div>{activity.description}</div>
-                                <div>{activity.city}, {activity.venue}</div>
-                            </Item.Description>
-                            <ItemExtra>
-                                <Button
-                                    as={Link} to={`/activities/${activity.id}`}
-                                    floated="right"
-                                    content='View'
-                                    color="blue"
-                                />
-                                <Button
-                                    name={activity.id}
-                                    loading={loading && target === activity.id}
-                                    onClick={(e) => handleDeleteActivity(e, activity.id)}
-                                    floated="right"
-                                    content='Delete'
-                                    color="red"
-                                />
-                                <Label basic content={activity.category} />
-                            </ItemExtra>
-                        </Item.Content>
-                    </Item>
-                ))}
-            </Item.Group>
-        </Segment>
+        <>
+            {groupedActivities.map(([group, activities]) => (
+                <Fragment key={group}>
+                    <Header sub color="teal">
+                        {group}
+                    </Header>
+                    {activities.map(activity => (
+                        <ActivityListItem key={activity.id} activity={activity} />
+                    ))}
+                </Fragment>
+            ))}
+        </>
     );
 });
--- client-app/src/features/activities/details/ActitvityDetail.tsx
+++ client-app/src/features/activities/details/ActitvityDetail.tsx
@@ -1,5 +1,5 @@
 import { observer } from 'mobx-react-lite';
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom';
 import { Button, Card, Image } from 'semantic-ui-react'
 import LoadingComponent from '../../../app/layout/LoadingComponent';
